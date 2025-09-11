742f357d1c2e8a942ca1c538eaa351ca6505d27c
Manthan <manthanmangukiya9048@gmail.com>
2025-09-11 18:19:49 +0530

    Merged @PathVariable and @RequestParam

--- Week 2/mvc/src/main/java/com/week2/mvc/mvc/controllers/EmployeeController.java
+++ Week 2/mvc/src/main/java/com/week2/mvc/mvc/controllers/EmployeeController.java
@@ -1,18 +1,18 @@
 package com.week2.mvc.mvc.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.week2.mvc.mvc.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
 
-// @RestController --> tells spring this class handles web requests ( HTTP calls )
-@RestController     // this prebuild
+@RestController
+@RequestMapping(path = "/employee")
 public class EmployeeController {
 
-    //MVC BASICS
-    // @GetMapping --> maps an HTTP GET request to this method
-    @GetMapping(path = "/getSuperSecretMessage")
-    public String getMySecretMessage(){
-        return "you got my secret message...";    // this msg is sent back as the HTTP response
+    @GetMapping("/manthan/{empid}/{empemail}")
+    public EmployeeDTO getEmployeeDetails(@PathVariable (name = "empid") Long id,
+                                          @RequestParam (required = false) String name,
+                                          @PathVariable (name = "empemail") String email,
+                                          @RequestParam (required = false) Integer age){
+        return new EmployeeDTO(id, name, email, age);
     }
 
 }
