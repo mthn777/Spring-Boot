159234286cb9dd6c0ed3fd5df7ef172a697f4b27
Manthan <manthanmangukiya9048@gmail.com>
2025-09-12 09:39:37 +0530

    2.3 Persistence Layer and JPA Repository

--- Week 2/mvc/src/main/java/com/week2/mvc/mvc/controllers/EmployeeController.java
+++ Week 2/mvc/src/main/java/com/week2/mvc/mvc/controllers/EmployeeController.java
@@ -1,35 +1,47 @@
 package com.week2.mvc.mvc.controllers;
 
-import com.week2.mvc.mvc.dto.EmployeeDTO;
+import com.week2.mvc.mvc.entities.EmployeeEnitity;
+import com.week2.mvc.mvc.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/employee")
 public class EmployeeController {
 
-    @GetMapping("/manthan/{empid}/{empemail}")
-    public EmployeeDTO getEmployeeDetails(@PathVariable (name = "empid") Long id,
-                                          @RequestParam (required = false) String name,
-                                          @PathVariable (name = "empemail") String email,
-                                          @RequestParam (required = false) Integer age){
-        return new EmployeeDTO(id, name, email, age);
+
+//=============================================================================================================
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
+
+
+//============================================================================================================
+
+
+    @GetMapping("/{empid}")
+    public EmployeeEnitity getEmployeeById(@PathVariable (name = "empid") Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping
+    public List<EmployeeEnitity> getAllEmployee(@RequestParam (required = false) Long age){
+        return employeeRepository.findAll();
+    }
+
+//============================================================================================================
 
     /*
     --> By default browser fire GET method
     --> to fire POST method, we mimic the frontend first
     */
 
-    @PostMapping("/{empid}")
-    public EmployeeDTO updateEmp(@PathVariable (name = "empid") Long id){
-        EmployeeDTO emp = new EmployeeDTO();
-
-        if(id == 1){
-            emp.setId(id);
-            emp.setAge(21);
-            emp.setName("Manthan");
-            emp.setEmail("manthan@gmail.com");
-        }
-        return emp;
+    @PostMapping
+    public EmployeeEnitity updateEmp(@RequestBody EmployeeEnitity newEntity){
+        return employeeRepository.save(newEntity);
     }
 }
